8b3392c27081dfa8951e36f52d36af3b31a1937b
imvik2000 <71227014+imvik2000@users.noreply.github.com>
2021-05-13 20:44:02 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,9 +1,19 @@
+
+def bill(total,tips):
+    return(tips/100)*total +total
+
+def split(persons,final_bills):
+    return(final_bills/persons)
+
 print("welcome to the tip calculator")
 total_bill=float(input("enter the total bill in $"))
 tip=int(input("enter the tip  in percentage without giving percent sign "))
-final_bill=(tip/100)*total_bill +total_bill
+
+final_bill=bill(total_bill,tip)
+
 person=int(input("how many person to split the bill?"))
 
-per_person_pay=final_bill/person
+per_person_pay=split(person,final_bill)
 per_person_pay= "{:.2f}".format(per_person_pay)  #  to round the bill amount with 2 decimal place 
 print(f"total amount each person will pay ${per_person_pay}")
+
